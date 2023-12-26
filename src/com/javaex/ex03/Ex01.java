8a15829a578ab82235e7a4f6895fef072ae46339
romiromib <raspberry.b85@gmail.com>
2023-12-26 16:26:09 +0900

    if~else 기본문법

---
+++ src/com/javaex/ex03/Ex01.java
@@ -0,0 +1,24 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int point = sc.nextInt();
+		
+		if(point >= 60) {
+			 //참일때
+			System.out.println("합격입니다.");
+			
+		}
+		
+		
+		sc.close();
+		
+	}
+	
+}
