81a5969498a76185ba6088200dd310912baf83cb
romiromib <raspberry.b85@gmail.com>
2023-12-26 18:14:31 +0900

    Switch~case

---
+++ src/com/javaex/ex03/Ex11.java
@@ -0,0 +1,19 @@
+package com.javaex.ex03;
+
+import java.util.Scanner;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int month = sc.nextInt();
+		
+		System.out.print(month + "월이면 "+ 31 +"일");
+		
+		sc.close();
+		
+
+	}
+
+}
